a819bdee35b994bf8ea1501f09754a12322bb4e8
Collins Murgor <vintoncollins702@gmail.com>
2023-09-05 09:00:56 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,5 @@
+import "./styles.css";
+import Image from "./Image.js";
+export default function App() {
+  return <Image />;
+}
---
+++ src/Image.js
@@ -0,0 +1,8 @@
+export default function Image() {
+  return(
+    <img 
+    src='https://i.imgur.com/ZFs192.jpg'
+    alt="'Floralis Generica' Eduardo Catalano: a gigantic metallic flower sculpture with reflective petals "
+    />
+  );
+}
